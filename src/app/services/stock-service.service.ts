424d61f70e991009de7655cc423e154c456e359f
AgilaD20 <98255105+AgilaD20@users.noreply.github.com>
2022-06-18 16:04:39 +0530

    Update stock-service.service.ts

--- src/app/services/stock-service.service.ts
+++ src/app/services/stock-service.service.ts
@@ -8,7 +8,7 @@ import { StockInfo } from '../models/StockInfo';
 })
 export class StockServiceService {
 
-  private Stock_URL: string="http://localhost:8081/stock-manager-service/api/v1.0/market/stock";
+  private Stock_URL: string="http://hackfse2-agila-gateway.westus.azurecontainer.io:8080/stock-manager-service/api/v1.0/market/stock";
 
   constructor(private http: HttpClient) { }
 
